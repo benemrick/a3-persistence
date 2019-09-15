0f4d1e85825bc3a2ab9848a3a576d0f1448845e6
benemrick <benemrick@gmail.com>
2019-09-15 18:20:39 -0400

    Add assets and UI refactor

--- public/client.js
+++ public/client.js
@@ -1,42 +1,152 @@
-// client-side js
-// run by the browser each time your view template is loaded
+//global
+let currEditIndex = 0;
 
-console.log('hello world :o');
-
-// our default array of dreams
-const dreams = [
-  'Find and count some sheep',
-  'Climb a really tall mountain',
-  'Wash the dishes'
-];
-
-// define variables that reference elements on our page
-const dreamsList = document.getElementById('dreams');
-const dreamsForm = document.forms[0];
-const dreamInput = dreamsForm.elements['dream'];
-
-// a helper function that creates a list item for a given dream
-const appendNewDream = function(dream) {
-  const newListItem = document.createElement('li');
-  newListItem.innerHTML = dream;
-  dreamsList.appendChild(newListItem);
+function setEditIndex(index) {
+  return currEditIndex = index;
 }
 
-// iterate through every dream and add it to our page
-dreams.forEach( function(dream) {
-  appendNewDream(dream);
-});
+const displayStars = function (rating) {
+  let div = `<div class="stars">`;
 
-// listen for the form to be submitted and add a new dream when it is
-dreamsForm.onsubmit = function(event) {
-  // stop our form submission from refreshing the page
-  event.preventDefault();
+  for (let i = 0; i < rating; i++) {
+    div += `<i class="fas fa-star"></i>`
+  }
+  div += `</div>`
 
-  // get dream value and add it to the list
-  dreams.push(dreamInput.value);
-  appendNewDream(dreamInput.value);
+  return div;
+}
 
-  // reset form 
-  dreamInput.value = '';
-  dreamInput.focus();
+const loadFavorites = async function () {
+  const resp = await fetch('/items', { method: 'GET' });
+  const data = await resp.json();
+  const favs = data.data;
+
+  let htmlCard = "";
+
+  for (let i = 0; i < 3; i++) {
+    if (favs[i]) {
+      let stars = displayStars(favs[i].rating);
+      htmlCard += `<div class="card"> 
+                       <div class="card-body">
+                        <h5 class="card-title">${favs[i].name}</h5>
+                        <p class="card-text"><medium class="text-muted">${favs[i].category}</medium></p>
+                        <p class="card-text">Price: $${favs[i].usd} | €${favs[i].eur}</p>
+                        <a class="btn btn-secondary stretched-link" href="${favs[i].link} target="_blank">Buy</a> 
+
+                    </div>
+                    <div class="card-footer bg-transparent">${stars}</div>
+                  </div>`;
+    }
+  }
+
+  document.getElementById("favorites").innerHTML = htmlCard;
 };
+
+const loadAllResults = async function () {
+  const resp = await fetch('/items', { method: 'GET' });
+  const data = await resp.json();
+  const favs = data.data;
+
+  let htmlTable = `<thead class="thead-dark">
+                        <tr>
+                          <th scope="col">Item</th>
+                          <th scope="col">Category</th>
+                          <th scope="col">USD</th>
+                          <th scope="col">EURO</th>
+                          <th colspan="3">Rating</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+      `;
+
+  for (let i = 0; i < favs.length; i++) {
+    let stars = displayStars(favs[i].rating);
+    htmlTable += `<tr>
+                        <td><a class="link" href="${favs[i].link} target="_blank">${favs[i].name}</a> </td>
+                        <td>${favs[i].category}</td>
+                        <td>$${favs[i].usd}</td>
+                        <td>€${favs[i].eur}</td>
+                        <td>${stars}</td>
+                        <td> <button id="editBtn${i}" type="button" class="btn btn-light" data-toggle="modal" data-target="#editItem" onclick="setEditIndex(${i})"> <i class="fas fa-edit"></i></button>
+                        <td> <button id="deleteBtn${i}" type="button" class="btn btn-light" onclick="deleteItem(${i})"> <i class="fas fa-trash"></i></button>
+                      </tr>
+        `;
+  }
+
+  htmlTable += `</tbody>`
+
+  document.getElementById("table").innerHTML = htmlTable;
+};
+
+const refresh = function () {
+  loadFavorites();
+  loadAllResults();
+}
+
+const newItem = function (e) {
+  // prevent default form action from being carried out
+  e.preventDefault()
+  const inName = document.querySelector('#name'),
+    inCategory = document.querySelector('#category'),
+    inRating = document.querySelector('#rating'),
+    inPrice = document.querySelector('#price'),
+    inLink = document.querySelector('#link'),
+    json = { name: inName.value, category: inCategory.value, rating: inRating.value, usd: inPrice.value, link: inLink.value },
+    body = JSON.stringify(json)
+  fetch('/submit', {
+    method: 'POST',
+    body
+  })
+    .then(function (response) {
+      refresh()
+      console.log(response)
+    })
+  return false
+}
+
+const deleteItem = function (arrPosition) {
+  const indexJson = { index: arrPosition };
+  const body = JSON.stringify(indexJson);
+  fetch('/delete', {
+    method: 'DELETE',
+    body
+  })
+    .then(function (response) {
+      refresh()
+      console.log(response)
+    })
+  return false
+}
+
+const editItem = function (e) {
+  e.preventDefault();
+
+  const inName = document.querySelector('#editName'),
+    inCategory = document.querySelector('#editCategory'),
+    inRating = document.querySelector('#editRating'),
+    inPrice = document.querySelector('#editPrice'),
+    inLink = document.querySelector('#editLink'),
+    json = { name: inName.value, category: inCategory.value, rating: inRating.value, usd: inPrice.value, link: inLink.value, index: currEditIndex },
+    body = JSON.stringify(json)
+
+  fetch('/update', {
+    method: 'PUT',
+    body
+  })
+    .then(function (response) {
+      refresh()
+      console.log(response)
+    })
+  return false
+}
+
+window.onload = function () {
+  refresh()
+
+  const button = document.getElementById('newBtn')
+  button.onclick = newItem
+
+  const editButton = document.getElementById('editBtn')
+  editButton.onclick = editItem
+
+}
